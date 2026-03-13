7f3a3bd317315fa1df2d4b1e835c2d4c0feaed60
Your Name <you@example.com>
2026-03-13 12:10:33 +0900

    first commit

---
+++ order/src/main/java/com/example/order/service/ProductOpenFeignService.java
@@ -0,0 +1,23 @@
+package com.example.order.service;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
+
+import com.example.order.domain.dto.ProductResponseDTO;
+
+/*
+localhost에서는
+@FeignClient(name = "product-service")
+k8s service
+@FeignClient(name = "product-service", url="http://product-service")
+*/
+@FeignClient(name = "product-service")
+public interface ProductOpenFeignService {
+
+    @GetMapping("/product/{productId}")
+    public ProductResponseDTO getProductId( @PathVariable("productId") Long productId, 
+                                            @RequestHeader("X-User-Email") String email) ;
+    
+}
